4a658075eb7c3be78678751a20da60d8459eb9de
Michele Sorbo <michele.sorbo@gmail.com>
2022-09-23 17:54:21 +0200

    as

---
+++ prog1/js/mio.js
@@ -0,0 +1,15 @@
+let qta = document.querySelectorAll(".qta");
+let prezzi = document.querySelectorAll("#prezzo");
+
+console.log(qta);
+console.log(prezzi[1]);
+
+qta.forEach((el, index) => {
+    //console.log(el.value);
+    el.addEventListener("change", function(e){
+        //console.log("Valore: "+el.value + " Indice:" +index + " Il prezzo è: " + prezzi[index].textContent);
+        let old_pr = prezzi[index].textContent;
+        console.log(old_pr.slice(old_pr.indexOf("€")+1));
+        prezzi[index].innerHTML = "€" + (Number(old_pr.slice(old_pr.indexOf("€")+1)) * Number(el.value));
+    });
+});
