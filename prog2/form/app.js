0c2f6c2d1d989f25abd2d3017e7b61d798728a01
Michele Sorbo <michele.sorbo@gmail.com>
2022-09-26 12:11:07 +0200

    js_st2

--- prog2/form/app.js
+++ prog2/form/app.js
@@ -1,5 +1,38 @@
 //Entry point
 
+/* Metodo classico
+let p_error = document.getElementById('error-message');
+p_error.innerHTML = "Errore vecchio metodo";
+*/
+
+//Metodo ad oggetti
+const conf = {
+    btnNext: document.getElementById('btn-next'),
+    btnPrev: document.getElementById('btn-prev'),
+    errorMessage: document.getElementById('error-message'),
+    formStepWrapper: document.getElementById('form-step-wrapper'),
+    indicatoriStep: document.querySelectorAll('#indicatori > div'),
+    formApp: document.forms['form-app'],
+    STEP_NUM: 3,
+    GAP: 20,
+    currentStep: 1,
+    stepValid: false
+}
+
+//conf.errorMessage.innerHTML = "Errore con oggetto conf";
+
+const stepValidatorEvent = e => {
+
+}
+
+const stepEvent = e => {
+    
+}
+
+
 export default () => {
     console.log('App in esecuzione...');
+    conf.btnNext.addEventListener('clik', stepValidatorEvent)
+    conf.btnNext.addEventListener('clik', stepEvent)
+    conf.btnPrev.addEventListener('click', stepEvent)
 }
