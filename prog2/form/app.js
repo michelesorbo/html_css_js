70b333a9989500afc7e96676aa86f015966b4d2b
Michele Sorbo <michele.sorbo@gmail.com>
2022-09-26 12:58:19 +0200

    js_st3

--- prog2/form/app.js
+++ prog2/form/app.js
@@ -1,4 +1,5 @@
 //Entry point
+//import checkForm from './validator'
 
 /* Metodo classico
 let p_error = document.getElementById('error-message');
@@ -26,6 +27,7 @@ const stepValidatorEvent = e => {
     e.preventDefault()
 
     //Controllo validità dati
+    checkForm(conf)
 }
 
 const stepEvent = e =>{
@@ -34,21 +36,56 @@ const stepEvent = e =>{
 
     //Gestire gli step
     if(e.target.id === 'btn-next'){
-        if(conf.currentStep === conf.STEP_NUM) return
+        if(conf.currentStep === conf.STEP_NUM || !conf.stepValid) return
         conf.currentStep++
-        //conf.stepValid = false
+        conf.stepValid = false
     }else{
         conf.currentStep--
     }
-
-    console.log(conf.currentStep);
-
+    
+    //console.log(conf.currentStep);
+    setIndicator(conf)
+    setStepForm(conf)
 }
 
 
 export default () => {
     console.log('App in esecuzione...');
-    conf.btnNext.addEventListener('clik', stepValidatorEvent)
-    conf.btnNext.addEventListener('clik', stepEvent)
+    conf.btnNext.addEventListener('click', stepValidatorEvent)
+    conf.btnNext.addEventListener('click', stepEvent)
     conf.btnPrev.addEventListener('click', stepEvent)
+}
+
+
+//Da scrivere in validator.js
+const checkForm = conf => {
+    console.log(conf.currentStep)
+    switch(conf.currentStep){
+        case 1: 
+            checkStep1(conf);
+            break;
+        case 2:
+            checkStep2(conf);
+            break;
+        case 3:
+            checkStep3(conf);
+            break;
+    } 
+}
+
+function checkStep1(conf){
+    resetErrorMessage(conf)
+    if(conf.formApp.elements['nome'].value.length >= 2 && conf.formApp.elements['cognome'].value.length >= 2){
+        return conf.stepValid = true
+    }
+    showErrorMessage(conf, 'Compilare i campi obbligatori')
+
+}
+
+function showErrorMessage(conf, msg){
+    conf.errorMessage.textContent = msg
+}
+
+function resetErrorMessage(conf){
+    conf.errorMessage.textContent = ''
 }
